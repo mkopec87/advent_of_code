4cc6a984a76357970feeeac989c9a3de78026d74
Mateusz Kopeć <3338226+mkopec87@users.noreply.github.com>
2022-12-30 23:45:55 +0100

    Solve day 1, 2020

--- 2020/01/2020_01.py
+++ 2020/01/2020_01.py
@@ -1,15 +1,24 @@
-from aocd import lines, submit
+from typing import Set
+
+from aocd import data, submit
+from itertools import combinations
+from math import prod
+
+
+def solve(numbers: Set[int], numbers_count: int, target_sum: int) -> int:
+    sets = combinations(numbers, numbers_count)
+    matching_sets = list(filter(lambda s: sum(s) == target_sum, sets))
+    assert len(matching_sets) == 1
+    matching_set = matching_sets[0]
+    return prod(matching_set)
 
 
 def main() -> None:
-    for line in lines:
-        pass
+    numbers = set(int(line) for line in data.splitlines())
+    print(len(numbers), "unique numbers read")
 
-    result_part1 = 0
-    # submit(result_part1, part="a")
-
-    result_part2 = 0
-    # submit(result_part2, part="b")
+    submit(solve(numbers, 2, 2020), part="a")
+    submit(solve(numbers, 3, 2020), part="b")
 
 
 if __name__ == "__main__":
