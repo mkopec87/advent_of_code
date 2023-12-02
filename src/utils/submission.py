24039a1c0a5777335c737637ec27b86b1b8723b5
Mateusz Kopeć <mateusz.kopec@ing.com>
2023-12-02 06:45:43 +0100

    Add debug info for submission

--- src/utils/submission.py
+++ src/utils/submission.py
@@ -6,10 +6,9 @@ from aocd import submit
 def submit_or_print(
     result_part1: Optional[Any], result_part2: Optional[Any], debug: bool
 ):
-    if debug:
-        print("Result part 1:", result_part1)
-        print("Result part 2:", result_part2)
-    else:
+    print("Result part 1:", result_part1)
+    print("Result part 2:", result_part2)
+    if not debug:
         if result_part1 is not None:
             submit(result_part1, part="a")
         if result_part2 is not None:
