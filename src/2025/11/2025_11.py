8c1c2696381ec04bda5568a68bdd3773354eb18f
mkopec87 <3338226+mkopec87@users.noreply.github.com>
2025-12-23 00:19:25 +0100

    2025 day 11 part 2

--- src/2025/11/2025_11.py
+++ src/2025/11/2025_11.py
@@ -1,5 +1,5 @@
-from collections import deque
-
+from functools import cache
+from typing import AbstractSet
 import networkx as nx
 from tqdm import tqdm
 
@@ -10,64 +10,49 @@ from src.utils.submission import submit_or_print
 def main(debug: bool) -> None:
     input_data = load_data(debug)
 
-    source_to_targets = {}
-    for line in input_data.splitlines():
-        s = line.split(":")
-        source = s[0].strip()
-        targets = set(s[1].strip().split(" "))
-        source_to_targets[source] = targets
+    graph = parse_input(input_data)
+    print(graph)
 
-    graph = nx.DiGraph()
-    for source, targets in source_to_targets.items():
-        for target in targets:
-            graph.add_edge(source, target)
-
-    # result_part1 = paths_count(graph, "you", "out")
-
-    source = "svr"
-    target = "out"
-
-    graphs = deque([graph])
-    while graphs:
-        g = graphs.pop()
-        cut_nodes = nx.minimum_node_cut(g, source, target)
-        print(g, len(cut_nodes), cut_nodes)
-        if len(cut_nodes) < 10:
-            g.remove_nodes_from(cut_nodes)
-            graphs.extend(
-                [
-                    g.subgraph(c).copy()
-                    for c in nx.connected_components(g.to_undirected())
-                ]
-            )
-
-    #
-    # inter_fft = paths_count(graph, "fft", "dac")
-    # inter_dac = paths_count(graph, "dac", "fft")
-    # print(inter_fft, inter_dac)
-    #
-    # fft = paths_count(graph, "svr", "fft")
-    # dac = paths_count(graph, "svr", "dac")
-    # print(fft, dac)
-    #
-    # fft_out = paths_count(graph, "fft", "out")
-    # dac_out = paths_count(graph, "dac", "out")
-    # print(fft_out, dac_out)
-
-    result_part1 = None
-    result_part2 = None
-
-    # nx.draw_networkx(graph)
-    # plt.show()
+    result_part1 = solve_part1(digraph=graph, start="you", end="out")
+    result_part2 = solve_part2(
+        digraph=graph, start="svr", end="out", via={"fft", "dac"}
+    )
 
     submit_or_print(result_part1, result_part2, debug)
 
 
-def paths_count(graph, start, end) -> int:
-    return sum(tqdm(1 for _ in nx.all_simple_paths(graph, start, end)))
+def parse_input(input_data: str) -> nx.DiGraph[str]:
+    graph: nx.DiGraph[str] = nx.DiGraph()
+    for line in input_data.splitlines():
+        s = line.split(":")
+        source = s[0].strip()
+        targets = set(s[1].strip().split(" "))
+        graph.add_edges_from((source, target) for target in targets)
+    return graph
+
+
+def solve_part1(digraph: nx.DiGraph[str], start: str, end: str) -> int:
+    return sum(tqdm(1 for _ in nx.all_simple_paths(digraph, start, end)))
+
+
+def solve_part2(
+    digraph: nx.DiGraph[str], start: str, end: str, via: AbstractSet[str]
+) -> int:
+    @cache
+    def path_count(start: str, end: str, visited: frozenset[str]) -> int:
+        if start == end and visited == via:
+            return 1
+        total = 0
+        for child in digraph.successors(start):
+            total += path_count(
+                start=child, end=end, visited=frozenset(visited | ({start} & via))
+            )
+        return total
+
+    return path_count(start=start, end=end, visited=frozenset())
 
 
 if __name__ == "__main__":
     debug_mode = True
-    debug_mode = False
+    # debug_mode = False
     main(debug_mode)
